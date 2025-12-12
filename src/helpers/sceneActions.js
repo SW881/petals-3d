7e2cea379dd9caed1ad2f91f5e7c452a45119b3e
shubh <shubhamw881@gmail.com>
2025-12-12 17:29:10 +0530

    fix: added helper functions for drawings

---
+++ src/helpers/sceneActions.js
@@ -0,0 +1,9 @@
+let actions = {
+    add: 'adding line to scene on pointer up',
+    remove: 'removing/erasing line from the scene on pointer up',
+    transforming:
+        'translate/rotate/scale/color/opacity change on selected lines',
+    linking: 'joining or rendering connected objects at once',
+    group_creation: '',
+    group_deletion: '',
+}
