9483c9d7b70521e4a944b8672c6c94b7491a4d9a
shubh <shubhamw881@gmail.com>
2025-12-12 17:28:28 +0530

    fix: no signup access

--- src/public-routes/PublicRoute.jsx
+++ src/public-routes/PublicRoute.jsx
@@ -1,24 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import { Navigate } from 'react-router-dom'
-import { UserAuth } from '../context/AuthContext'
-import { dashboardStore } from '../hooks/useDashboardStore'
+import React from 'react'
 
 const PublicRoute = ({ children }) => {
-    const { validateSession } = UserAuth()
-    const { session, loading } = dashboardStore((state) => state)
-
-    useEffect(() => {
-        validateSession()
-    }, [])
-
-    // if (loading) return <>Loading.. 456</>
-    if (loading) return <></>
-
-    if (session) {
-        return <Navigate to="/folders" replace />
-    } else {
-        return children
-    }
+    return children
 }
 
 export default PublicRoute
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { createBrowserRouter, Navigate } from 'react-router-dom'
 
-import SignIn from '../components/signin/SignIn'
 import Editor from '../components/editor/Editor'
 
 import PublicRoute from '../public-routes/PublicRoute'
-import ProtectedRoute from '../protected-routes/ProtectedRoute'
 
 export const router = createBrowserRouter([
-    // Protected Routes
     {
-        element: <ProtectedRoute />,
+        element: <PublicRoute />,
         children: [
             {
                 path: '/*',
@@ -21,15 +18,5 @@ export const router = createBrowserRouter([
                 element: <Editor />,
             },
         ],
-    },
-
-    // Non-Protected Routes
-    {
-        path: '/sign-in',
-        element: (
-            <PublicRoute>
-                <SignIn />
-            </PublicRoute>
-        ),
     },
 ])
